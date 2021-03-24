68350b9a3700a1b58aa60400ec41f570b7b0d9c4
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-03-24 16:12:46 +1100

    Cleaned up cardinal directions to 1-16

--- src/csv2numpy.py
+++ src/csv2numpy.py
@@ -1,5 +1,5 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 
 def csv2np(data):
@@ -9,13 +9,36 @@ def csv2np(data):
 
     df_np = df[df["Location"] == "Uluru"].to_numpy()
 
-    idx_no = np.where(df_np == 'No')
-    idx_yes = np.where(df_np == 'Yes')
-    idx_nan = np.where(df_np == 'nan')
+    idx_no = np.where(df_np == "No")
+    idx_yes = np.where(df_np == "Yes")
+    idx_nan = np.where(df_np == "nan")
 
     df_np[idx_no] = False
     df_np[idx_yes] = True
     df_np[idx_nan] = np.nan
+
+    cardinal = [
+        "N",
+        "NNE",
+        "NE",
+        "ENE",
+        "E",
+        "ESE",
+        "SE",
+        "SSE",
+        "S",
+        "SSW",
+        "SW",
+        "WSW",
+        "W",
+        "WNW",
+        "NW",
+        "NNW",
+    ]
+
+    for i, c in enumerate(cardinal):
+        idx_c = np.where(df_np == c)
+        df_np[idx_c] = i + 1
 
     return df_np
 
@@ -24,5 +47,6 @@ if __name__ == "__main__":
     data = "../data/weatherAUS.csv"
 
     weather_np = csv2np(data)
-    print(weather_np)
-
+    print(
+        weather_np[0]
+    )
