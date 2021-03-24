f1b66a51834900a1a2afc243c2dd405830905bd9
Aman Chokshi <amanchokshi.iisc@gmail.com>
2021-03-24 15:45:43 +1100

    clean yes, no, nans

--- src/csv2numpy.py
+++ src/csv2numpy.py
@@ -1,18 +1,28 @@
 import pandas as pd
+import numpy as np
 
 
 def csv2np(data):
 
     df = pd.read_csv(data)
-    df['Date'] = pd.to_datetime(df['Date'])
-    print(df[ df['Location']=='Uluru'] )
+    df["Date"] = pd.to_datetime(df["Date"])
 
-    return df.to_numpy()
+    df_np = df[df["Location"] == "Uluru"].to_numpy()
+
+    idx_no = np.where(df_np == 'No')
+    idx_yes = np.where(df_np == 'Yes')
+    idx_nan = np.where(df_np == 'nan')
+
+    df_np[idx_no] = False
+    df_np[idx_yes] = True
+    df_np[idx_nan] = np.nan
+
+    return df_np
 
 
 if __name__ == "__main__":
-    data = "./data/weatherAUS.csv"
+    data = "../data/weatherAUS.csv"
 
     weather_np = csv2np(data)
-    print( weather_np[:,1] )
-    print( weather_np.shape )
+    print(weather_np)
+
